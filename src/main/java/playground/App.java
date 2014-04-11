c21c2958d1f66179e8cef43b6d631707762fa0a9
SiminaTofan <s.tofan@levi9.com>
2014-04-11 16:19:09 +0300

    comit of initial user - by stash

--- src/main/java/playground/App.java
+++ src/main/java/playground/App.java
@@ -12,8 +12,8 @@ public class App
     public static void main( String[] args )
     {
         ClassA objectA = new ClassA("classA", 4.3);
-        objectA.setLabel("modification for BranchA - simulation again user");
-        objectA.setAmount(1.0);
+        objectA.setLabel("modification for BranchA - initial user - user stash");
+        objectA.setAmount(8.0);
         
         ClassB objectB = new ClassB("dfhaksdfjhas", true);
         objectB.setNegation(true);
