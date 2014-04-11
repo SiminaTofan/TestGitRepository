209aafdc6446eba0b75abdd9a365726ecec54048
SiminaTofan <s.tofan@levi9.com>
2014-04-11 15:55:04 +0300

    commit on master - user1

--- src/main/java/playground/App.java
+++ src/main/java/playground/App.java
@@ -12,11 +12,11 @@ public class App
     public static void main( String[] args )
     {
         ClassA objectA = new ClassA("classA", 4.3);
-        objectA.setLabel("modification for BranchA - second user");
-        objectA.setAmount(5.0);
+        objectA.setLabel("modification for BranchA - master user");
+        objectA.setAmount(6.0);
         
         ClassB objectB = new ClassB("dfhaksdfjhas", true);
-        objectB.setNegation(false);
+        objectB.setNegation(true);
         
         System.out.println(objectA.toString());
         objectB.setNegation(false);
