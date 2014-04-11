1c4b8ba241942d0b294b85edd350ffd82d8433d2
SiminaTofan <s.tofan@levi9.com>
2014-04-11 16:10:18 +0300

    another commit for simulation user - use stash

--- src/main/java/playground/App.java
+++ src/main/java/playground/App.java
@@ -12,8 +12,8 @@ public class App
     public static void main( String[] args )
     {
         ClassA objectA = new ClassA("classA", 4.3);
-        objectA.setLabel("modification for BranchA - first user");
-        objectA.setAmount(7.0);
+        objectA.setLabel("modification for BranchA - simulation again user");
+        objectA.setAmount(1.0);
         
         ClassB objectB = new ClassB("dfhaksdfjhas", true);
         objectB.setNegation(true);
